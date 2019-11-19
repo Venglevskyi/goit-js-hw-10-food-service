b40913393e5c2ea56739865dd94075fa87e6f402
Roman <Roman.Vengl1@gmail.com>
2019-11-19 21:17:53 +0200

    change task

--- src/components/changeTheme.js
+++ src/components/changeTheme.js
@@ -5,15 +5,16 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
+let changeTheme = localStorage.getItem('theme') || Theme.LIGHT;
+refs.body.classList.add(changeTheme);
+
 function handleChangeTheme() {
   if (refs.switch.checked === true) {
-    localStorage.setItem('theme', Theme.DARK);
-    refs.body.classList.add(Theme.DARK);
-    refs.body.classList.remove(Theme.LIGHT);
+    refs.body.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK)
   } else {
+    refs.body.classList.replace(Theme.DARK, Theme.LIGHT);
     localStorage.setItem('theme', Theme.LIGHT);
-    refs.body.classList.add(Theme.LIGHT);
-    refs.body.classList.remove(Theme.DARK);
   }
 }
 refs.switch.addEventListener('change', handleChangeTheme);
