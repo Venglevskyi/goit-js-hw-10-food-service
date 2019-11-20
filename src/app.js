0e4449dfa6a27445d33f8e9e2650bb779a04a02c
Roman <Roman.Vengl1@gmail.com>
2019-11-20 12:18:58 +0200

    task last version

--- src/app.js
+++ src/app.js
@@ -1,7 +1,32 @@
-import refreshList from './components/menu';
+import menu from './components/menu';
 import database from './database/menu.json';
 import refs from './utils/refs';
-import handleChangeTheme from './components/changeTheme';
 
-refreshList(database, refs.menu);
-handleChangeTheme();
+menu(database, refs.menu);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const initTheme = localStorage.getItem('theme') || Theme.LIGHT;
+if (initTheme === Theme.LIGHT) {
+  refs.switch.checked = false;
+} else {
+  refs.switch.checked = true;
+}
+refs.body.classList = initTheme;
+
+function setTheme(currentTheme) {
+  refs.body.classList = currentTheme;
+  localStorage.setItem('theme', currentTheme);
+}
+
+function handleChangeTheme() {
+  if (refs.switch.checked === true) {
+    setTheme(Theme.DARK);
+  } else {
+    setTheme(Theme.LIGHT);
+  }
+}
+refs.switch.addEventListener('change', handleChangeTheme);
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,8 +1,8 @@
 import template from '../templates/menu-items.hbs';
 
-function refreshList(data, root) {
+function menu(data, root) {
   const htmlMarkup = data.map(e => template(e)).join('\n');
   root.insertAdjacentHTML('afterbegin', htmlMarkup);
 }
 
-export default refreshList;
+export default menu;
